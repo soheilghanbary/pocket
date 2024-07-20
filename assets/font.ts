ebb79703782a16e1d1e3cf065f3f9aeaa890872e
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-07-20 12:01:54 +0330

    add hono & prisma & shadcn & react-query

---
+++ assets/font.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google'
+
+export const font = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
