f5d86d328b5ba35c6176ca8edcd1be967b8698c2
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-07-20 13:13:50 -0700

    add IranSans font

--- assets/font.ts
+++ assets/font.ts
@@ -1,7 +1,47 @@
-import { Inter } from 'next/font/google'
+import localFont from "next/font/local";
 
-export const font = Inter({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter'
-})
+// export const font = Inter({
+//   subsets: ['latin'],
+//   weight: ['300', '400', '500', '600', '700', '800', '900'],
+//   variable: '--font-inter'
+// })
+
+export const font = localFont({
+  src: [
+    {
+      path: "./fonts/IRANSansX-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IRANSansX-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IRANSansX-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IRANSansX-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IRANSansX-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IRANSansX-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IRANSansX-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
