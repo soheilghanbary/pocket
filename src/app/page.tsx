6f9439381aaf6342facfb6d8a3fc8736110b645c
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-07-28 13:28:39 +0330

    update more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-async function sayHello() {
-  const res = await fetch('http://localhost:3000/api/hello');
-  const result = await res.json();
-  return result;
-}
-
-export default async () => {
-  const hello = await sayHello();
+export default () => {
   return (
     <section className="p-8">
       <h1 className="text-center font-extrabold text-2xl text-foreground">
         I'm Ready to Create!
       </h1>
-      <p>{JSON.stringify(hello)}</p>
     </section>
   );
 };
